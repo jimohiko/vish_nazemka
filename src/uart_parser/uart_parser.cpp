e001ba819fab30cf62d59cf377887d66494cb243
jimohiko <145903554+jimohiko@users.noreply.github.com>
2024-06-22 23:21:25 +0300

    парсер покеты целиком но всё ешё с задержкой

--- src/uart_parser/uart_parser.cpp
+++ src/uart_parser/uart_parser.cpp
@@ -7,27 +7,34 @@ char uartBuf[30];
 bool comandUpdeteFlag = false;
 #if UART_PARSER_TEST == true
 void uartParserTest() {
-    if (parserSerial.available() > 0) {
-        int amount = parserSerial.readBytesUntil(';', uartBuf, 30);
-        uartBuf[amount] = NULL;
+    if (parserSerial.available() > 0 && comandUpdeteFlag == false) {
+        if (parserSerial.find("GPS:")) {
+            int amount = parserSerial.readBytesUntil(';', uartBuf, 30);
+            uartBuf[amount] = NULL;
+            Serial.print(amount);
+            comandUpdeteFlag = true;
+        }
+        Serial.print(" text: ");
         Serial.println(uartBuf);
-        comandUpdeteFlag = true;
     }
-    
+
     char* ptrs[10];       // указатели на строки
     int count = 0;        // счётчик подстрок
     char* offset = uartBuf;   // указатель для работы
-    while (true) {
-        ptrs[count++] = offset;         // запоминаем указатель
-        offset = strchr(offset, ',');   // ищем новую запятую
-        if (offset) {                   // если это не NULL
-            *offset = NULL;               // заменяем запятую на нулл
-            offset++;                     // продолжаем
-        } else break;                     // иначе покидаем цикл
-    }
+    if (comandUpdeteFlag == true) {
+        while (true) {
+            ptrs[count++] = offset;         // запоминаем указатель
+            offset = strchr(offset, ',');   // ищем новую запятую
+            if (offset) {                   // если это не NULL
+                *offset = NULL;               // заменяем запятую на нулл
+                offset++;                     // продолжаем
+            } else break;                     // иначе покидаем цикл
+        }
 
-    if(comandUpdeteFlag == true){
-        for (int i = 0; i < count; i++) Serial.println(ptrs[i]);
+        for (int i = 0; i < count; i++) {
+            Serial.print("\t");
+            Serial.println(ptrs[i]);
+        }
         comandUpdeteFlag = false;
     }
 }
